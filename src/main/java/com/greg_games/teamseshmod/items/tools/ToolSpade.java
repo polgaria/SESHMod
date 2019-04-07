e979a518f178f99111ce7678d1b11eb22054f1b0
Gregory Hortex Mikel Laire <greggamesproductions@gmail.com>
2019-04-07 20:46:46 -0300

    Added 3 of the 5 Sesh Tools
    yuh

---
+++ src/main/java/com/greg_games/teamseshmod/items/tools/ToolSpade.java
@@ -0,0 +1,28 @@
+package com.greg_games.teamseshmod.items.tools;
+
+import com.greg_games.teamseshmod.Main;
+import com.greg_games.teamseshmod.init.ModItems;
+import com.greg_games.teamseshmod.util.IHasModel;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemSpade;
+import net.minecraft.item.Item.ToolMaterial;
+
+public class ToolSpade extends ItemSpade implements IHasModel {
+
+	public ToolSpade(String name, ToolMaterial material)
+	{
+		super(material);
+		setUnlocalizedName(name);
+		setRegistryName(name);
+		setCreativeTab(CreativeTabs.TOOLS);
+		
+		ModItems.ITEMS.add(this);
+	}
+
+	@Override
+	public void registerModels() 
+	{
+		Main.proxy.registerItemRenderer(this, 0, "inventory");
+	}
+}

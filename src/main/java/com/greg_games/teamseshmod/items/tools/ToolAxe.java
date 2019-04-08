f98c32d2084c9f7b5164261dd862345b8701f188
Gregory Hortex Mikel Laire <greggamesproductions@gmail.com>
2019-04-08 19:27:40 -0300

    Added TeamSESH Merch set as Armor
    + 2 new tools and renamed the material of the tools to "SESH"

---
+++ src/main/java/com/greg_games/teamseshmod/items/tools/ToolAxe.java
@@ -0,0 +1,29 @@
+package com.greg_games.teamseshmod.items.tools;
+
+import com.greg_games.teamseshmod.Main;
+import com.greg_games.teamseshmod.init.ModItems;
+import com.greg_games.teamseshmod.util.IHasModel;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.item.ItemAxe;
+
+public class ToolAxe extends ItemAxe implements IHasModel {
+	
+	public ToolAxe(String name, ToolMaterial material)
+	{
+		super(material, Float.MAX_VALUE, Float.MAX_VALUE);
+		setUnlocalizedName(name);
+		setRegistryName(name);
+		setCreativeTab(CreativeTabs.TOOLS);
+		
+		ModItems.ITEMS.add(this);
+	}
+
+	@Override
+	public void registerModels() 
+	{
+		Main.proxy.registerItemRenderer(this, 0, "inventory");
+	}
+
+}
---
+++ src/main/java/com/greg_games/teamseshmod/items/tools/ToolHoe.java
@@ -0,0 +1,30 @@
+package com.greg_games.teamseshmod.items.tools;
+
+import com.greg_games.teamseshmod.Main;
+import com.greg_games.teamseshmod.init.ModItems;
+import com.greg_games.teamseshmod.util.IHasModel;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.item.ItemHoe;
+
+public class ToolHoe extends ItemHoe implements IHasModel 
+{
+	
+	public ToolHoe(String name, ToolMaterial material)
+	{
+		super(material);
+		setUnlocalizedName(name);
+		setRegistryName(name);
+		setCreativeTab(CreativeTabs.TOOLS);
+		
+		ModItems.ITEMS.add(this);
+	}
+
+	@Override
+	public void registerModels() 
+	{
+		Main.proxy.registerItemRenderer(this, 0, "inventory");
+	}
+
+}
